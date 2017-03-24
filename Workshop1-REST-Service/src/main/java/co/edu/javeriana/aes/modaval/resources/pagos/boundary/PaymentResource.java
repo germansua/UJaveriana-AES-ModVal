42a78dfb6510bb85a434e21fb51542bff9a9fbfa
German Alonso Suarez Guerrero <gersua@GERMANs-MacBook-Pro.local>
2017-03-24 16:12:25 -0500

    Docker refactor

--- Workshop1-REST-Service/src/main/java/co/edu/javeriana/aes/modaval/resources/pagos/boundary/PaymentResource.java
+++ Workshop1-REST-Service/src/main/java/co/edu/javeriana/aes/modaval/resources/pagos/boundary/PaymentResource.java
@@ -1,6 +1,7 @@
 package co.edu.javeriana.aes.modaval.resources.pagos.boundary;
 
-import co.edu.javeriana.aes.modaval.resources.pagos.entity.*;
+import co.edu.javeriana.aes.modaval.resources.pagos.entity.Factura;
+import co.edu.javeriana.aes.modaval.resources.pagos.entity.Resultado;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
