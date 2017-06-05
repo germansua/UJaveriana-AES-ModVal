9ef2ee3de57783a9f3892f1ea7b5d70e15bc02e2
German Alonso Suárez Guerrero <gersua@Germans-MacBook-Pro.local>
2017-06-04 23:55:51 -0500

    Implementing client

--- simple-rmi-java/RMIClient/src/main/java/co/edu/javeriana/aes/modval/rmi/Main.java
+++ simple-rmi-java/RMIClient/src/main/java/co/edu/javeriana/aes/modval/rmi/Main.java
@@ -1,8 +1,19 @@
 package co.edu.javeriana.aes.modval.rmi;
 
+import java.rmi.Naming;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+        EchoServer remoteService =
+                (EchoServer) Naming.lookup("rmi://java-rmi-server:7875/EchoService");
+        while (true) {
+            remoteService.echo("Hello World at " + System.currentTimeMillis());
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException ex) {
+                System.out.println(ex);
+            }
+        }
     }
 }
