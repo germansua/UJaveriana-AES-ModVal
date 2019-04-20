28e50a7d071d0898386d0ec6b337dfd052dddff4
German Alonso Suárez Guerrero <gersua@Germans-MacBook-Pro.local>
2019-04-19 19:05:04 -0500

    Organizing Repo

--- ABServiceTesting/WildflySwarmService/src/main/java/co/edu/javeriana/aes/modval/config/ApplicationConfig.java
+++
@@ -1,8 +0,0 @@
-package co.edu.javeriana.aes.modval.config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class ApplicationConfig extends Application {
-}
