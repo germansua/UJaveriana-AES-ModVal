7d2e3a792215c4f5b8a03a92448c35c60d14a9a7
German Alonso Suárez Guerrero <gersua@Germans-MacBook-Pro.local>
2019-04-19 22:13:06 -0500

    Removing unwanted projects

--- workshops/w1-second-rest-service/src/main/java/co/edu/javeriana/aes/modval/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package co.edu.javeriana.aes.modval;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api/resources")
-public class AppConfig extends Application {
-    
-}
--- workshops/w1-second-rest-service/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mvn clean package
-cp target/etb-mobile.war Payara
-docker-compose up --build
-rm Payara/etb-mobile.war
