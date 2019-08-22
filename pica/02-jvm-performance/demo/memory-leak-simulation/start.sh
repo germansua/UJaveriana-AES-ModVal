5df57cc877862fa1afee41bb23e62bb9032bbcd6
German Alonso Suárez Guerrero <gersua@Germans-MacBook-Pro.local>
2019-08-21 23:22:13 -0500

    Adding HeapDumpOnOutOfMemoryError JAVA_TOOL_OPTIONS

--- pica/02-jvm-performance/demo/memory-leak-simulation/start.sh
+++ pica/02-jvm-performance/demo/memory-leak-simulation/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-10.0.1.jdk/Contents/Home
-export JAVA_TOOL_OPTIONS="-XX:+UnlockCommercialFeatures -XX:+FlightRecorder"
+export JAVA_TOOL_OPTIONS="-XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/tmp/memory-leak-simulation.hprof"
 ./gradlew clean build
-java -Xms24m -Xmx240m -jar build/libs/memory-leak-simulation.jar $1
+java -Xms24m -Xmx24m -jar build/libs/memory-leak-simulation.jar $1
