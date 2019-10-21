51f7321a1eb224a21cf45efd205f62f48be8724c
German Alonso Suarez Guerrero <gersua@gmail.com>
2019-10-20 19:53:39 -0500

    Adding to ProductCatalogue Microprofile Config(ConfigProperty) & FaultTolerance (Retry, Fallback, CircuitBreaker, Fallback)

--- pica/04-jakartaee-microprofile/demo/ProductCatalogue/src/main/java/co/edu/javeriana/pica/jeemp/resources/catalogue/CatalogueService.java
+++ pica/04-jakartaee-microprofile/demo/ProductCatalogue/src/main/java/co/edu/javeriana/pica/jeemp/resources/catalogue/CatalogueService.java
@@ -1,10 +1,16 @@
 package co.edu.javeriana.pica.jeemp.resources.catalogue;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.LocalBean;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
+import javax.inject.Inject;
 import javax.json.JsonObject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -12,15 +18,27 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 @LocalBean
 @TransactionAttribute(TransactionAttributeType.SUPPORTS)
 public class CatalogueService {
 
+    private static Logger LOGGER = Logger.getLogger(CatalogueService.class.getName());
+
     @PersistenceContext(name = "catalogue-pu")
     private EntityManager entityManager;
+
+    @Inject
+    @ConfigProperty(name = "currency.exchange.url")
+    private String currencyExchangeServiceUrl;
+
+    @Inject
+    @ConfigProperty(name = "currency.exchange.port")
+    private int currencyExchangeServicePort;
 
     private Client client;
     private WebTarget target;
@@ -33,13 +51,22 @@ public class CatalogueService {
                 .readTimeout(2, TimeUnit.SECONDS)
                 .build();
 
-        target = client.target("http://currency-exchange:8080").path("/api/resources/exchange");
+        target = client
+                .target(String.format("http://%s:%d", currencyExchangeServiceUrl, currencyExchangeServicePort))
+                .path("/api/resources/exchange");
     }
 
+    @Retry(delay = 500, jitter = 150, maxRetries = 5, maxDuration = 3500)
+    @Fallback(fallbackMethod = "fetchAllFallback")
     public List<Catalogue> fetchAll() {
         return entityManager
                 .createNamedQuery("Catalogue.findAll", Catalogue.class)
                 .getResultList();
+    }
+
+    private List<Catalogue> fetchAllFallback() {
+        LOGGER.severe("Using fetchAllFallback");
+        return Collections.emptyList();
     }
 
     private Catalogue fetchById(long id) {
@@ -51,6 +78,8 @@ public class CatalogueService {
                 .orElse(new Catalogue());
     }
 
+    @CircuitBreaker(requestVolumeThreshold = 5, failureRatio = 0.2, delay = 1000L, successThreshold = 5)
+    @Fallback(fallbackMethod = "fetchByIdPriceByCurrencyFallback")
     public Catalogue fetchByIdPriceByCurrency(long id, String currency) {
         Catalogue catalogue = fetchById(id);
         double price = catalogue.getPrice();
@@ -67,6 +96,11 @@ public class CatalogueService {
         }
 
         return new Catalogue(catalogue.getId(), catalogue.getBrand(), catalogue.getProduct(), price);
+    }
+
+    public Catalogue fetchByIdPriceByCurrencyFallback(long id, String currency) {
+        LOGGER.severe(String.format("Using fetchByIdPriceByCurrencyFallback with id %d and currency %s", id, currency));
+        return new Catalogue("", "", 0.0);
     }
 
     @PreDestroy
--- pica/04-jakartaee-microprofile/demo/ProductCatalogue/startup.sh
+++ pica/04-jakartaee-microprofile/demo/ProductCatalogue/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build . -t javeriana-aes-pica/product-catalogue-img:1.0.0
-docker run -d --network product-catalogue-net -p 8080:8080 -p 4848:4848 --name product-catalogue javeriana-aes-pica/product-catalogue-img:1.0.0
+docker run -d -e CURRENCY_EXCHANGE_URL='currency-exchange' -e CURRENCY_EXCHANGE_PORT=8080 --network product-catalogue-net -p 8080:8080 -p 4848:4848 --name product-catalogue javeriana-aes-pica/product-catalogue-img:1.0.0
